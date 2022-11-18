a3c3a345e97c1042cdcf7f80b0896a25068bd8af
DaniilShd <ds_const@mail.ru>
2022-11-18 19:32:13 +0300

    New commit

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"path/filepath"
 
 	"github.com/DaniilShd/WebApp/pkg/handlers"
 )
@@ -11,9 +12,13 @@ const portNumber = ":8080"
 
 func main() {
 	http.HandleFunc("/", handlers.Home)
+
+	fmt.Println(filepath.Dir("/template"))
+
 	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Starting%s", portNumber))
 
 	_ = http.ListenAndServe(portNumber, nil)
+
 }
